a159c2f91800a82613fae403e063d030d2bb01d2
STATSOFT\b.nowak <b.nowak@statsoft.pl>
2026-07-15 19:52:38 +0200

    #1.5 prepare solution for deployment

--- Program.cs
+++ Program.cs
@@ -1,18 +1,23 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
     app.MapOpenApi();
-}
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseHttpsRedirection();
+
+app.MapHealthChecks("/healthz");
 
 var summaries = new[]
 {
